fc25621476d23328f7a0988dd601d4df99345b9b
bibek <crazy764gaming@gmail.com>
2026-01-24 21:49:41 +0530

    Add chess piece rendering to board display
    Introduced a board representation and functions to display chess pieces on the board using turtle graphics. Pieces are now drawn in different colors for white and black, and the board is initialized with standard chess starting positions.

--- chess.py
+++ chess.py
@@ -1,12 +1,30 @@
 import turtle
 
 pen = turtle.Pen()
+turtle.tracer(0)
+pen.hideturtle()
+
+board = [["br","bh","bb","bq","bk","bb","bh","br"],
+         ["bp","bp","bp","bp","bp","bp","bp","bp"],
+         ["__","__","__","__","__","__","__","__"],
+         ["__","__","__","__","__","__","__","__"],
+         ["__","__","__","__","__","__","__","__"],
+         ["__","__","__","__","__","__","__","__"],
+         ["wp","wp","wp","wp","wp","wp","wp","wp"],
+         ["wr","wh","wb","wq","wk","wb","wh","wr"],
+         ]
+
+white_piece = ["wp","wr","wh","wb","wq","wk","wb","wh","wr"]
+black_piece = ["br","bh","bb","bq","bk","bb","bh","br","bp"]
 
 left = -240
 right = 240
 
 def main():
     chess_board()
+    pieces_location()
+    turtle.done()
+
 
 def small_square(x,y,colour):
     pen.penup()
@@ -29,6 +47,23 @@ def chess_board():
             y = right - 60 * raw
             colour = colours[(raw + col) % 2]
             small_square(x,y,colour)
+
+def pieces_location():
+    initial_x = -215
+    initial_y = 205
+    for raw in range(8):
+        for col in range(8):
+            x = initial_x + 60 * col
+            y = initial_y - 60 * raw
+            put_piece(x, y, raw, col)
+
+def put_piece(x,y,raw,col):
+    pen.penup()
+    pen.goto(x,y)
+    pen.pendown()
+    if board[raw][col] != "__" :
+        pen.color("#99FFEA") if board[raw][col] in white_piece else pen.color("#8E15AB")
+        pen.write((board[raw][col]),font=("Arial",12,"bold"))
 
 if __name__ == "__main__":
     main()
