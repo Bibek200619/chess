38140f5c35b86308635a566196a45e12fc6513b6
bibek <crazy764gaming@gmail.com>
2026-01-27 01:30:22 +0530

    Add pawn move highlighting and refactor turn logic
    Introduced functions to highlight possible pawn moves on the board. Refactored turn management by renaming 'turn' to 'turns' and improving logic for determining the current player's pieces. Added visual feedback for pawn movement and prepared structure for future move highlighting for other pieces.

--- chess.py
+++ chess.py
@@ -25,7 +25,7 @@ black_piece = ["br","bh","bb","bq","bk","bb","bh","br","bp"]
 left = -240
 right = 240
 
-turn = [white_piece, black_piece]
+turns = [white_piece, black_piece]
 selected = None
 count = 0
 
@@ -78,27 +78,32 @@ def put_piece(x,y,raw,col):
 
 def move_piece(x , y ):
     global selected, count
-    s = 0
+    turn = turns[0 if count % 2 == 0 else 1]
 
     square = xy_location(x,y)
     if square == None:
         return
+    else:
+        raw, col = square
+        highlight_next_move(board[raw][col], raw, col,turn)
 
-    raw, col = square
 
     if selected is None:
-        if board[raw][col] != "__" and board[raw][col] in turn[0 if count % 2 == 0 else 1]:
+        if board[raw][col] != "__" and board[raw][col] in turn:
             selected = (raw,col)
         return
 
     source_raw , source_col = selected
+
+    if board[raw][col] in turn and board[source_raw][source_col] in turn:
+        return
+
 
     board[raw][col] = board[source_raw][source_col]
     board[source_raw][source_col]  = "__"
     selected = None
     refresh()
     count = count + 1
-
 
 
 def refresh():
@@ -120,6 +125,48 @@ def print_board(board):
         for col in range(8):
             print(board[raw][col],end=" ")
         print()
+
+def highlight_next_move(piece,raw,col,turn):
+    if piece in turn:
+        green_it(piece,raw,col)
+
+def green_it(piece,raw,col):
+    if piece in ["bp","wp"]:
+        if (piece == "wp" and raw == 6) or (piece == "bp" and raw == 1) :
+            next_raw = raw + 2 if piece == "bp" else raw - 2
+
+            not_moved = True
+        else:
+            next_raw = raw + 1 if piece == "bp" else raw - 1
+            not_moved = False
+
+        if next_raw < 0 or next_raw >= 8:
+            return
+        
+        x = -240 + (col * 60)
+        y = 240 - (next_raw * 60)
+        pawn_highlights(x, y, not_moved,piece)
+
+    elif piece in ["bw","wr"] :
+        pass
+
+def pawn_highlights(x, y, not_moved,nm):
+    if not_moved:
+        paint_highlights(x, y+60 if nm == "bp" else y-60 )
+        paint_highlights(x, y)
+    else:
+        paint_highlights(x,y)
+
+def paint_highlights(x,y):
+    pen.penup()
+    pen.goto(x, y)
+    pen.pendown()
+    pen.color("green")
+    pen.begin_fill()
+    for _ in range(4):
+        pen.forward(60)
+        pen.right(90)
+    pen.end_fill()
 
 if __name__ == "__main__":
     main()
